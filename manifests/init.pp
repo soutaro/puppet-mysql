e3f22f537aa15a446cca8e8c2db8b64efb83e132
Soutaro Matsumoto <matsumoto@soutaro.com>
2014-02-27 04:49:46 +0900

    Set strict mode

--- manifests/init.pp
+++ manifests/init.pp
@@ -94,7 +94,7 @@ class mysql {
 
   exec { 'mysql-tzinfo-to-sql':
     command     => "mysql_tzinfo_to_sql /usr/share/zoneinfo | \
-      mysql -u root mysql -P ${mysql::config::port} -S ${mysql::config::socket}",
+      mysql -u root mysql -P ${mysql::config::port} -S ${mysql::config::socket} -f",
     provider    => shell,
     creates     => "${mysql::config::datadir}/.tz_info_created",
     subscribe   => Exec['wait-for-mysql'],
